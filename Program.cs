6eff7adb178c8a6af295d00d87c1ff2906eae537
Azat Khafizov <imrider@mail.ru>
2024-01-30 23:51:31 +0500

    Add object's

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Привет МИР!");
+
+int value = 100;
+Console.WriteLine(value);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
